dd12d7cbcf0cc9dfc243038d4eab1e5838eab248
Mauricio Pena <mauricio.pena@gmail.com>
2015-08-26 09:55:11 -0300

    Módulo 8

---
+++ Módulo 8/sample_app/test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  setup do
+    get signup_path
+  end
+
+  test "invalid signup information" do    
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    assert_not is_logged_in?
+  end
+
+  test "valid signup information" do    
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
+
+end
